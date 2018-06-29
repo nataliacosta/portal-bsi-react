779713370cc3c768426f8095d8ceb3a6a0854307
nataliacosta <nataliacosta@Natalias-MacBook-Air.local>
2018-06-29 17:23:37 -0300

    created header components

---
+++ src/Header.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import iBSI from './assets/images/iBSI.png';
+import iAlunos from './assets/images/iAlunos.png';
+import iDisciplinas from './assets/images/iDisciplinas.png';
+import iInstitucional from './assets/images/iInstitucional.png';
+import iNoticias from './assets/images/iNoticias.png';
+import iOportunidades from './assets/images/iOportunidades.png';
+import iProfessores from './assets/images/iProfessores.png';
+import iSecretaria from './assets/images/iSecretaria.png';
+import iTCC from './assets/images/iTCC.png';
+
+const Header = () => {
+    return (
+        <header className="d-none d-lg-block">
+            <div className="container-big">
+                <nav className="d-flex align-items-center justify-content-center">
+                    <div className="container">
+                        <div className="row">
+                            <a href="/bsi" className="col-3 pt-0 pl-0 pbr-10 pb-0">
+                                <div className="icone bsi w-100 h-100">
+                                    <p className="subtitle">
+                                        <img src={iBSI} className="icon"  alt="bsi"/>
+                                        <br/>BACHARELADO  EM<br/>
+                                        <span className="subtitleGrande">SISTEMAS DE<br/>INFORMAÇÃO</span>
+                                    </p>
+                                </div>
+                            </a>
+                            <div className="col-6 p-0">
+                                <div className="container">
+                                    <div className="row linha-maior">
+                                        <a href="/disciplinas" className="col-3 pt-0 pl-0 pbr-10">
+                                            <div className="icone disciplinas w-100 h-100">
+                                                <p className="subtitle">
+                                                    <img className="icon" src={iDisciplinas} className="icon"  alt="disciplinas"/>
+                                                    <br/>DISCIPLINAS
+                                                </p>
+                                            </div>
+                                        </a>
+                                        <a href="/alunos" className="col-6 pt-0 pl-0 pbr-10">
+                                            <div className= "icone alunos w-100 h-100">
+                                                <p className="subtitle">
+                                                    <img src={iAlunos} className="icon"  alt="alunos"/>
+                                                    <br/>ALUNOS
+                                                </p>
+                                            </div>
+                                        </a>
+                                        <a href="/tcc" className="col-3 pt-0 pl-0 pbr-10">
+                                            <div className= "icone tcc w-100 h-100">
+                                                <p className="subtitle">
+                                                    <img className="icon" src={iTCC} className="icon"  alt="Trabalho de Conclusão de Curso"/>
+                                                    <br/>TCC
+                                                </p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div className="row">
+                                        <a href="/professores" className="col-3 pt-0 pl-0 pbr-10 pb-0">
+                                            <div className= "icone professores w-100 h-100">
+                                                <p className="subtitle">
+                                                    <img src={iProfessores} className="icon"  alt="Professores" />
+                                                    <br/>PROFESSORES
+                                                </p>
+                                            </div>
+                                        </a>
+                                        <a href="/oportunidades" className="col-3 pt-0 pl-0 pbr-10 pb-0">
+                                            <div className= "icone oportunidades w-100 h-100">
+                                                <p className="subtitle">
+                                                    <img className="icon" src={iOportunidades} className="icon"  alt="oportunidades"/>
+                                                    <br/>OPORTUNIDADES
+                                                </p>
+                                            </div>
+                                        </a>
+                                        <a href="/secretaria" className="col-3 pt-0 pl-0 pbr-10 pb-0">
+                                            <div className= "icone secretaria w-100 h-100">
+                                                <p className="subtitle">
+                                                    <img className="icon" src={iSecretaria} className="icon"  alt="secretaria"/>
+                                                    <br/>SECRETARIA
+                                                </p>
+                                            </div>
+                                        </a>
+                                        <a href="/institucional" className="col-3 pt-0 pl-0 pbr-10 pb-0">
+                                            <div className= "icone institucional w-100 h-100">
+                                                <p className="subtitle">
+                                                    <img src={iInstitucional} className="icon"  alt="institucional"/>
+                                                    <br/>INSTITUCIONAL<br/>>EIA-CCET-UNIRIO
+                                                </p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="/noticias" className="col-3 pt-0 pl-0 pbr-10 pb-0">
+                                <div className="icone noticias w-100 h-100 semMargem">
+                                    <p className="subtitle subtitleGrande">
+                                        <img src={iNoticias} className="icon"  alt="notícias"/>
+                                        <br/>NOTÍCIAS
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/Header_Small.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import iBSI from './assets/images/iBSI.png';
+
+const Header_Small = () => {
+    return (
+        <header className="header-min d-xs-block d-lg-none">
+            <div className="container-small h-100">
+                <div className="d-flex align-items-center justify-content-between h-100 ml-3 mr-3">
+                    <i className="mdi mdi-3x mdi-menu text-white" onClick="openNav()"></i>
+                    <p className="subtitle-small">
+                        <img src={iBSI} className="bsi-pequeno" alt="bsi"/>
+                        <br/>BACHARELADO EM
+                        <br/>
+                        <span className="subtitle-smallGrande">
+                            SISTEMAS DE
+                            <br/>INFORMAÇÃO
+                        </span>
+                    </p>
+                    <div width="40px"></div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header_Small;
